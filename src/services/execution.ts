323e86912ccd5fb08c3603f4cee4d7c18be8c976
bsnk-dev <61131051+bsnk-dev@users.noreply.github.com>
2021-08-02 00:13:19 -0500

    init

--- src/services/execution.ts
+++ src/services/execution.ts
@@ -124,35 +124,116 @@ class ExecutionService {
     /**
      * So the vm has access to the in memory volume
      */
-    const patchedRequire = (`
-      (() => {
-        const _filesystemVolume = require('fs');
-        const oldRequire = Object.assign(require);
-
-        patchedRequire.cache = Object.create(null);
-
-        process.stdout = {};
-        process.stderr = {};
-
-        function patchedRequire(name) {
-            if (name == 'fs') return _filesystemVolume;
-
-            if (!name.startsWith('.') && !name.startsWith('/')) {
-              return oldRequire(name);
-            }
-
-            if (!(name in patchedRequire.cache)) {     
-                let code = _filesystemVolume.readFileSync(name, 'utf8');
-                let module = {exports: {}};
-                patchedRequire.cache[name] = module;
-                let wrapper = Function("require, exports, module", code);
-                wrapper(patchedRequire, module.exports, module);
-            }
-            return patchedRequire.cache[name].exports;
-        }
-
-        require = patchedRequire;
-      })();
+    const patch = (`
+    (() => {
+      const realRequire = Object.assign(require);
+    
+      const isFilePathPattern = /^[./]/;
+      const hasExtensionPattern = /\\.\\w+$/;
+    
+      const isDirectory = (
+        fs,
+        directoryPath,
+      ) => (
+        fs.existsSync(directoryPath)
+        && fs.lstatSync(directoryPath).isDirectory()
+      );
+    
+      const specifierPattern = /^((?:@[\\da-z][\\w.-]+\\/)?[\\da-z][\\w.-]+)(\\/.+)?$/;
+      const getBareSpecifier = (
+        modulePath,
+      ) => modulePath.match(specifierPattern).slice(1, 3);
+    
+      process.stdout = {};
+      process.stderr = {};
+    
+      const vm_1 = realRequire("vm");
+      const path_1 = realRequire("path");
+      const module_1 = realRequire("module");
+      const loaders = {
+          '': undefined,
+          '.js': undefined,
+          '.json': undefined,
+      };
+      /**
+       * Extensionless JS files
+       * Takes priority over .js file
+       */
+      loaders[''] = function (newModule, sourceCode, makeRequire, filename, id) {
+          const moduleWrappedSourceCode = module_1.wrap(sourceCode);
+          // Reference: https://github.com/nodejs/node/blob/master/lib/internal/modules/cjs/loader.js#L1028
+          vm_1.runInThisContext(moduleWrappedSourceCode, {
+              filename: \`fs-require://\${id}\${filename}\`,
+              lineOffset: 0,
+              displayErrors: true,
+          })(newModule.exports, makeRequire(newModule), newModule, path_1.basename(filename), path_1.dirname(filename));
+      };
+      loaders['.js'] = loaders[''];
+      loaders['.json'] = function (newModule, sourceCode) {
+          newModule.exports = JSON.parse(sourceCode);
+      };
+      function resolveImplicitExtension(fs, filePath) {
+          for (const extension of ['', '.js', '.json']) {
+              const filePathWithExtension = filePath + extension;
+              if (fs.existsSync(filePathWithExtension)) {
+                  return {
+                      extension,
+                      filePath: filePathWithExtension,
+                  };
+              }
+          }
+          return null;
+      }
+    
+      let idCounter = 0;
+      const createFsRequire = (mfs) => {
+          idCounter += 1;
+          const fsRequireId = idCounter;
+          const moduleCache = new Map();
+          function makeRequireFunction(parentModule) {
+              const require = (modulePath) => {
+                  var _a, _b;
+                  if (!isFilePathPattern.test(modulePath)) {
+                      const [moduleName, moduleSubpath] = (_a = getBareSpecifier(modulePath)) !== null && _a !== void 0 ? _a : [];
+                      if (moduleName === 'fs') {
+                          if (moduleSubpath) {
+                              throw new Error(\`Cannot find module '\${modulePath}'\`);
+                          }
+                          return mfs;
+                      }
+                      return realRequire(modulePath);
+                  }
+                  let filename = path_1.resolve(path_1.dirname(parentModule.filename), modulePath);
+                  let pathExtension = (_b = filename.match(hasExtensionPattern)) === null || _b === void 0 ? void 0 : _b[0];
+                  if (!pathExtension) {
+                      const resolvedPath = isDirectory(mfs, filename)
+                          ? resolveImplicitExtension(mfs, path_1.join(filename, 'index'))
+                          : resolveImplicitExtension(mfs, filename);
+                      if (!resolvedPath) {
+                          throw new Error(\`Cannot find module '\${modulePath}'\`);
+                      }
+                      filename = resolvedPath.filePath;
+                      pathExtension = resolvedPath.extension;
+                  }
+                  if (moduleCache.has(filename)) {
+                      return moduleCache.get(filename).exports;
+                  }
+                  const newModule = new module_1(filename, parentModule);
+                  newModule.filename = filename;
+                  const sourceCode = mfs.readFileSync(filename).toString();
+                  loaders[pathExtension](newModule, sourceCode, makeRequireFunction, filename, fsRequireId);
+                  moduleCache.set(filename, newModule);
+                  return newModule.exports;
+              };
+              require.id = fsRequireId;
+              return require;
+          }
+    
+          return makeRequireFunction({filename: '/'});
+      };
+      
+      require = createFsRequire(require('fs'));
+    })();
     `);
 
     const packageInfo = volume.readFileSync('/package.json').toString();
@@ -164,7 +245,7 @@ class ExecutionService {
       config.json.execution.vms.sandboxDirectory.production :
       config.json.execution.vms.sandboxDirectory.development;
 
-    const vmExports = vm.run(`${patchedRequire} ${indexJS}`, join(sandboxDirectory, instance.volumeID || ''));
+    const vmExports = vm.run(`${patch} ${indexJS}`, join(sandboxDirectory, instance.volumeID || ''));
 
     const cachedVM =
       this.inMemoryVMs.find((vm) => {
